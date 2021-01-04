70f4f06b7fd25beaca1dc5e3043d450716e79ca7
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2021-01-04 22:11:24 +0100

    Add frontend app

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -10,7 +10,15 @@ exports.handler = function (event, context, callback) {
 
   // send user response
   const send = (body) => {
-    callback(null, { statusCode: 200, body: JSON.stringify(body) });
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+      body: JSON.stringify(body),
+    });
   };
 
   // perform api call
